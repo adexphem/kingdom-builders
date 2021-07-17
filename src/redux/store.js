a46a04850d08376d0a0be60ef5dad84f0f6facf1
phemmy <adexphem4real@gmail.com>
2021-07-17 20:26:19 +0100

    update the store structure

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,22 @@
-import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from "redux-devtools-extension";
-import thunk from 'redux-thunk';
-import rootReducer from './reducers';
+import { createStore, applyMiddleware } from "redux";
+import { composeWithDevTools } from "redux-devtools-extension/logOnlyInProduction";
+import thunk from "redux-thunk";
+import rootReducer from "./reducers";
+
+const composeEnhancers = composeWithDevTools({
+  // options like actionSanitizer, stateSanitizer
+});
 
 const initialState = {};
-
 const middleware = [thunk];
 
 const store = createStore(
   rootReducer,
   initialState,
-  composeWithDevTools(applyMiddleware(...middleware))
+  composeEnhancers(
+    applyMiddleware(...middleware)
+    // other store enhancers if any
+  )
 );
 
 export default store;
