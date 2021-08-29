6c40b113d122194e54f6cfef8fbae931431946d3
phemmy <adexphem4real@gmail.com>
2021-08-29 02:03:14 +0100

    create-make-payment-modal - add logic to handle excess payment ui

--- src/pages/KingdomBuilders/SingleView.js
+++ src/pages/KingdomBuilders/SingleView.js
@@ -1,25 +1,20 @@
 import React, { Fragment, useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useParams } from "react-router-dom";
-import { useForm } from "react-hook-form";
-import { yupResolver } from "@hookform/resolvers/yup";
-import * as yup from "yup";
 
 import {
   fetchBuilderList,
   fetchPledgerAmountPaid,
   fetchPaymentDetailsById,
-  addPaymentById,
 } from "../../redux/actions/kingdomBuilderAction";
 
-import { Button, PaymentProgression, Input } from "../../components";
+import { Button, PaymentProgression } from "../../components";
 import { CheckHexIcon, SadIcon } from "../../components/icons";
 import TableRows from "../../components/TableRows";
 import InfoCard from "./InfoCard";
-import Modal from "../../components/Modal";
-import AmountCardSm from "../../components/AmountCardSm";
 
 import { SmEmpty } from "../../components/TableRows/EmptyHeaders";
+import PaymentModal from "./PaymentModal";
 
 import {
   MainContainer,
@@ -34,17 +29,7 @@ import {
   StyledHexIcon,
   EmptyContent,
   EmptyContentText,
-  PaymentModalContent,
-  PMCCenter,
-  PMCName,
-  PMCData,
-  PMCForm,
-  PMCFormButton,
 } from "./styles";
-
-const PaymentFormSchema = yup.object().shape({
-  amount: yup.string().required("Valid amount/number is required."),
-});
 
 const Index = () => {
   const { id } = useParams();
@@ -54,14 +39,6 @@ const Index = () => {
   const pledgerAmountPaid = useSelector((state) => state.kingdomBuilders.pledgerTotalAmountPaid);
   const [selectedPledger, updateSelectedPledger] = useState("");
   const [isOpenPaymentModal, setIsOpenPaymentModal] = useState(false);
-  const [isPaymentLoading, setIsPaymentLoading] = useState(false);
-
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    reset,
-  } = useForm({ resolver: yupResolver(PaymentFormSchema) });
 
   const handleDataFetch = () => {
     updateSelectedPledger(pledgers.find((item) => item.id.toString() === id.toString()));
@@ -93,62 +70,9 @@ const Index = () => {
     handleDataFetch();
   }, [selectedPledger]); // eslint-disable-line react-hooks/exhaustive-deps
 
-  const onSubmitHandler = async (data) => {
-    const { amount } = data;
-
-    if (amount.length < 0) return;
-    setIsPaymentLoading(true);
-    try {
-      await dispatch(addPaymentById({ amount_paid: amount, id: selectedPledger.id, entered_by: "admin" }));
-      dispatch(fetchBuilderList());
-      setIsPaymentLoading(false);
-    } catch (e) {
-      console.error("Failed to login: ", e);
-      setIsPaymentLoading(false);
-    }
-
-    reset();
-  };
-
   return (
     <>
-      {isOpenPaymentModal ? (
-        <Modal title="Payment Record Update" icon="papermoney" onClose={closeModal}>
-          <PaymentModalContent>
-            <>
-              <PMCCenter>{selectedPledger.parishes_id}</PMCCenter>
-              <PMCName>{selectedPledger.name}</PMCName>
-            </>
-            <PMCData>
-              <AmountCardSm title="Amount Pledge" amount={selectedPledger.amount_pledge} />
-              <AmountCardSm title="Amount Paid" amount={selectedPledger.total_amount_paid} />
-              <AmountCardSm
-                title="Pending"
-                amount={selectedPledger.amount_pledge - selectedPledger.total_amount_paid}
-              />
-            </PMCData>
-            <PMCForm id="loginForm" onSubmit={handleSubmit(onSubmitHandler)}>
-              <Input
-                placeholder="Enter the amount"
-                label="Amount"
-                name="amount"
-                id="amount"
-                autoFocus
-                disabled={false}
-                classes={`border-bottom`}
-                errorMessage={""}
-                error={errors}
-                type="number"
-                {...register("amount")}
-              />
-
-              <PMCFormButton>
-                <Button id="loginFormBtn" button_text="Submit" type="submit" inProgress={isPaymentLoading} />
-              </PMCFormButton>
-            </PMCForm>
-          </PaymentModalContent>
-        </Modal>
-      ) : null}
+      {isOpenPaymentModal ? <PaymentModal closeModal={closeModal} /> : null}
       <MainContainer>
         <PageTitle>
           <StyledProfileName>
--- src/pages/KingdomBuilders/styles.js
+++ src/pages/KingdomBuilders/styles.js
@@ -1,5 +1,5 @@
 import { up } from "styled-breakpoints";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 export const FlexWrapper = styled.div`
   display: flex;
@@ -244,71 +244,5 @@ export const EmptyContentText = styled.h1`
 
   ${up("lg")} {
     font-size: 1.5rem;
-  }
-`;
-
-export const PaymentModalContent = styled.div`
-  text-align: left;
-  line-height: 3rem;
-`;
-
-export const common = css`
-  width: 100%;
-  text-align: center;
-  line-height: 2.3rem;
-  font-weight: ${({ theme }) => theme.fontWeight.regular};
-  font-family: ${({ theme }) => theme.fonts.primary};
-`;
-
-export const PMCCenter = styled.div`
-  ${common}
-  font-size: 1rem;
-  font-weight: 600;
-  color: ${({ theme }) => theme.color.color578e53};
-`;
-
-export const PMCName = styled.div`
-  ${common}
-  font-size: 1.5rem;
-  color: ${({ theme }) => theme.color.pallyGrey};
-`;
-
-export const PMCData = styled(FlexWrapper)`
-  flex-direction: column;
-  justify-content: space-between;
-  margin-top: 30px;
-
-  ${up("lg")} {
-    flex-direction: row;
-
-    > div {
-      max-width: 30%;
-    }
-  }
-`;
-
-export const PMCForm = styled.form`
-  display: flex;
-  font-family: ${({ theme }) => theme.fonts.primary};
-  padding: 20px 30px;
-  margin-top: 20px;
-  flex-direction: column;
-  border-radius: 3px;
-  background: rgba(196, 196, 196, 0.13);
-`;
-
-export const PMCFormButton = styled.div`
-  display: flex;
-  justify-content: center;
-  font-family: ${({ theme }) => theme.fonts.secondary};
-  width: 160px;
-  margin: 15px auto;
-
-  ${up("lg")} {
-    width: 50%;
-  }
-
-  button {
-    padding: 25px 0;
   }
 `;
